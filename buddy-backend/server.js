f5c3fe33c2a674628714b041c56076dbf66cb5bd
Cooldev0824 <Cooldev0824@gmail.com>
2025-02-05 10:07:44 -0500

    Attach Backend

---
+++ buddy-backend/server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const cors = require("cors"); // Import CORS
+const app = express();
+const PORT = process.env.PORT || 3000;
+const axios = require("axios");
+
+// Middleware
+app.use(cors()); // Use CORS middleware
+app.use(express.json()); // Middleware to parse JSON bodies
+
+app.post("/", (req, res) => {
+    const { messageText } = req.body; // Adjusted to use req.body directly
+    console.log(messageText)
+    axios.post(
+        "https://api.nomi.ai/v1/nomis/75bdd368-eef0-47f4-a6a3-dcc8a736ae93/chat",
+        {
+            messageText: messageText,
+        },
+        {
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: "46cdebc8-c044-4adf-b651-f6e90fd2f5ed",
+            },
+        }
+    )
+    .then((response) => {
+        console.log(response.data.replyMessage.text);
+        res.send(response.data);
+    })
+    .catch((err) => console.log(err));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
